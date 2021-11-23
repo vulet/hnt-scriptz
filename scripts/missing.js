6f96dbd211c576bf0c4705242ae322845be4761a
Rocket Sloth <87209309+RocketSloth@users.noreply.github.com>
2021-11-23 14:35:35 -0600

    Update missing.js

--- scripts/missing.js
+++ scripts/missing.js
@@ -7,7 +7,7 @@ const ascii =
       |_ __ _|_    _  _  __ o |_)_|_ _
       | || | |_   _> (_  |  | |   |_ /_
 `;
-const INVALID_LENGTH = 'This script only allows for 11 or 23 words.';
+const INVALID_LENGTH = 'This script only allows for 10 or 22 words.';
 log(ascii);
 
 async function findWord() {
@@ -17,18 +17,18 @@ async function findWord() {
   ];
   const { mnemonic, missing } = await inquirer.prompt(questions);
   const words = mnemonic.split(' ');
-  if (words.length !== 11 && words.length !== 23) throw new Error(INVALID_LENGTH);
+  if (words.length !== 10 && words.length !== 22) throw new Error(INVALID_LENGTH);
   words.map((word) => {
     if (wordlist.indexOf(word) === -1) throw new Error(`Invalid word: ${word}`);
   });
   if (!Address.isValid(missing)) throw new Error(`Invalid address: ${missing}`);
-  for (let order = 0; order < (words.length + 1); order++) {
+  for (let order = 0; order < (words.length + 2); order++) {
     for (let i = 0; i < wordlist.length; i++) {
       words.splice(order, 0, wordlist[i]);
       const keypair = await Keypair.fromWords(words).catch(() => {});
       keypair ? address = keypair.address.b58 : address = [];
       if (address !== missing) {
-        words.splice(order, 1);
+        words.splice(order, 2);
       } else return log('\n', 'Address:', address, '\n', 'Words:', words);
     }
   }
